cbd04a2ae0a3c2f84625eb7123502888514f3103
Lyarvo4ka <Lyarvo4ka@gmail.com>
2019-08-14 19:45:01 +0300

    ++

--- include/raw/oleraw.h
+++ include/raw/oleraw.h
@@ -4,7 +4,16 @@
 
 namespace RAW
 {
-	struct olecf_file_header
+	const uint32_t END_OF_CHAIN =	 0xFFFFFFFE;
+	const uint32_t FREE_SECTORS =	 0xFFFFFFFF;
+	const uint32_t FAT_SECTORS  =	 0xFFFFFFFD;
+	const uint32_t DIFSECT		=	 0xFFFFFFFC;
+
+	const uint32_t FAT_VAL_SIZE = 4;
+	const uint32_t FIRST_109_SECTORS = 109;
+
+
+	struct OLE_header
 	{
 		/* The magic signature of the OLECF compound file
 		 * Consists of 8 bytes
@@ -21,34 +30,34 @@ namespace RAW
 		/* The minor version of the OLECF format used
 		 * Consists of 2 bytes
 		 */
-		uint8_t minor_format_version[2];
+		uint16_t minor_format_version;
 
 		/* The major verion of the OLECF format used
 		 * also used refered to as DLL version
 		 * Consists of 2 bytes
 		 */
-		uint8_t major_format_version[2];
+		uint16_t major_format_version;
 
 		/* The byte order within the file
 		 * 0xfffe indicates little endian
 		 * 0xfeff indicates big endian
 		 * Consists of 2 bytes
 		 */
-		uint8_t byte_order[2];
+		uint16_t byte_order;
 
 		/* The sector (block) size within the file
 		 * also refered to as sector shift
 		 * Consists of 2 bytes
 		 * default is 9 indicating 2^9 = 512
 		 */
-		uint8_t sector_size[2];
+		uint16_t sector_shift;
 
 		/* The short (mini) sector (block) size within the file
 		 * also refered to as mini sector shift
 		 * Consists of 2 bytes
 		 * default is 6 indicating 2^6 = 64
 		 */
-		uint8_t short_sector_size[2];
+		uint16_t minifat_sector_shift;
 
 		/* Reserved
 		 * Consists of 2 bytes
@@ -71,12 +80,12 @@ namespace RAW
 		/* The number of SAT sectors
 		 * Consists of 4 bytes
 		 */
-		uint8_t number_of_sat_sectors[4];
+		uint32_t number_of_fat_sectors;
 
 		/* The sector identifier of the sector that contains the root directory
 		 * Consists of 4 bytes
 		 */
-		uint8_t root_directory_sector_identifier[4];
+		uint32_t root_directory_sector_start;
 
 		/* Signature used for transactioning
 		 * Consists of 4 bytes
@@ -87,36 +96,135 @@ namespace RAW
 		 * otherwise a short-sector stream is used
 		 * Consists of 4 bytes
 		 */
-		uint8_t sector_stream_minimum_data_size[4];
+		uint32_t sector_stream_minimum_data_size;
 
 		/* The sector identifier of the sector that contains the first SSAT
 		 * Consists of 4 bytes
 		 */
-		uint8_t ssat_sector_identifier[4];
+		uint32_t minifat_sector_start;
 
 		/* The number of SSAT sectors
 		 * Consists of 4 bytes
 		 */
-		uint8_t number_of_ssat_sectors[4];
+		uint32_t number_of_minifat_sectors;
 
 		/* The sector identifier of the sector that contains the first MSAT
-		 * Consists of 4 bytes
+		 * Consists of 4 bytes. If file is smaller than 7 Mb the value must 0xFFFFFFFE(END_OF_CHAIN)
 		 */
-		uint8_t msat_sector_identifier[4];
+		uint32_t dif_sector_start;
 
 		/* The number of MSAT sectors
 		 * Consists of 4 bytes
 		 */
-		uint8_t number_of_msat_sectors[4];
+		uint32_t number_of_msat_sectors;
 
 		/* The first 109 MSAT sector identifiers
 		 * Consists of 436 bytes
 		 */
-		uint8_t msat[436];
+		uint32_t msat[FIRST_109_SECTORS];
 	};
 
 
 	//1. ssat_sector_identifier
+
+	class Sector
+	{
+		DataArray data_;
+		uint64_t number_ = 0;
+	public:
+		Sector(const uint32_t sector_number = 0 , const uint32_t sector_size = default_sector_size)
+			:data_(sector_size)
+			, number_(sector_number)
+		{
+
+		}
+
+		ByteArray data()
+		{
+			return data_.data();
+		}
+		void setSectorNumber(const uint64_t sector_number)
+		{
+			number_ = sector_number;
+		}
+		uint64_t number() const
+		{
+			return number_;
+		}
+		uint64_t offset() const
+		{
+			return number() * data_.size();
+		}
+		uint32_t size() const
+		{
+			return data_.size();
+		}
+
+	};
+
+	class DIF_Sector
+	{
+		DataArray sector_data_;
+	public:
+		DIF_Sector(const uint32_t sector_size = default_sector_size)
+			: sector_data_(sector_size)
+		{
+
+		}
+		ByteArray data()
+		{
+			return sector_data_.data();
+		}
+		uint32_t size() const
+		{
+			return sector_data_.size() - FAT_VAL_SIZE;
+		}
+		uint32_t next_chain() const
+		{
+			return sector_data_.data()[sector_data_.size() - 1];
+		}
+	};
+
+
+
+	class OLE_FAT
+	{
+		std::vector<uint32_t> table_;
+	public:
+		void addSector( Sector & sector)
+		{
+//			std::transform(std::begin()table_.begin(), table_.end(),)
+			for (auto i = 0; i < sector.size(); i += FAT_VAL_SIZE)
+				table_.push_back(sector.data()[i]);
+		}
+	};
+
+	void testOLE()
+	{
+		auto filename = LR"(d:\ole_test\2012-08-05-10-29-0000344.doc)";
+		//	auto filename = LR"(d:\ole_test\bigdoc.doc)";
+
+		OLE_FAT fat_table;
+
+		IO::File file(filename);
+		file.OpenRead();
+		IO::DataArray data_array(default_sector_size);
+		file.ReadData(data_array);
+
+		auto ole_header = (RAW::OLE_header*)data_array.data();
+		for (auto iSector = 0; iSector < FIRST_109_SECTORS; ++iSector)
+		{
+			Sector sector(ole_header->msat[iSector]);
+			file.setPosition(sector.offset());
+			file.ReadData(sector.data(), sector.size());
+			fat_table.addSector(sector);
+		}
+		if (ole_header->dif_sector_start != END_OF_CHAIN)
+		{
+			
+			// read using DIF sectors
+		}
+	}
 
 	class OleRAW
 		: public DefaultRaw
@@ -135,9 +243,9 @@ namespace RAW
 			this->setPosition(start_offset);
 			this->ReadData(sector);
 
-			olecf_file_header* pOLE_header = (olecf_file_header*)sector.data();
+			OLE_header* pOLE_header = (OLE_header*)sector.data();
 
-			//pOLE_header->ssat_sector_identifier
+
 
 			return 0;
 		}
