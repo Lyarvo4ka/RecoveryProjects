9128f4b6b0e2b8ca797749143a8ae9b54010017e
Lyarvo4ka <Lyarvo4ka@gmail.com>
2020-04-06 16:55:35 +0300

    ++

---
+++ RawRecovery/signatureitem.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "treeitem.h"
+
+enum SignatureItemType : int
+{
+	kSignatureItem, kCategoryItem
+};
+
+class SignatureAdapter
+{
+
+};
+
+class CategoryFolder
+{
+public:
+};
+
+class SignatureItem
+{
+public:
+		
+};
---
+++ RawRecovery/treeitem.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <qvariant.h>
+
+template < class AdapterType >
+class TreeItem
+{
+	std::unique_ptr<AdapterType> adapter_;
+	QVector<TreeItem*> childItems_;
+	TreeItem* parentItem_ = nullptr;
+public:
+	explicit TreeItem(std::unique_ptr<AdapterType> adapter, TreeItem* parentItem = nullptr)
+		: adapter_(std::move(adapter))
+		, parentItem_(parentItem)
+	{
+
+	}
+	~TreeItem()
+	{
+		qDeleteAll(childItems_);
+	}
+
+	void appendChild(TreeItem* child)
+	{
+		childItems_.append(child);
+	}
+	TreeItem* child(int row)
+	{
+		if (row < 0 || row >= childItems_.size())
+			return nullptr;
+		return childItems_.at(row);
+
+	}
+	int childCount() const
+	{
+		return childItems_.count();
+	}
+	int columnCount() const
+	{
+		return DeviceItemNames::kCount;
+	}
+	AdapterType* getDeviceAdapter()
+	{
+		return adapter_.get();
+	}
+
+	int row() const
+	{
+		if (parentItem_ != nullptr)
+			return parentItem_->childItems_.indexOf(const_cast<TreeItem*>(this));
+
+		return 0;
+
+	}
+	TreeItem* parentItem()
+	{
+		return parentItem_;
+	}
+
+
+};
